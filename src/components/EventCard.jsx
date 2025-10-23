d2fcd4bfdbaafef055e191ea718ce68bb7be8876
saulmackenzie <smacke300@caledonian.ac.uk>
2025-10-23 10:53:37 +0100

    EventCard data

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-export default function EventCard({event}) {
-    return (
-        <div class="card" style={{ width: '18rem' }}>
-          <img src={event?.image ?? "https://tse4.mm.bing.net/th/id/OIP.ndCKdMbJTMQkzLuC72moswHaFE?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3"} class="card-img-top" alt={event?.event + " Image" ?? "Placeholder Image"} />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-    );
+export default function EventCard({event}) { 
+  return (
+    <div class="card" style={{ width: '18rem' }}>
+      <img src={event?.image ?? "https://tse4.mm.bing.net/th/id/OIP.ndCKdMbJTMQkzLuC72moswHaFE?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3"} class="card-img-top" alt={event?.event + " Image" ?? "Placeholder Image"} />
+      <div class="card-body">
+        <h5 class="card-title">{event?.event ?? "Event Name"}</h5>
+        <p class="card-text">{event.startTime} to {event.endTime}, {event.date}</p>
+        <p class="card-text">{event?.description ?? "Lorem ipsum dolor sit amet."}</p>
+        <p class="card-text">Location: {event.location}</p>
+        <a href={"event/" + event._id} class="btn btn-primary">View Event</a>
+      </div>
+    </div>
+  );
 }
